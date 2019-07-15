21c23943170c56ca111dcb1b79a8ccd3c565b3fd
hemonugi <hemonugi@sakh.com>
2019-07-16 00:42:14 +1100

    add config example for project deploying

---
+++ config.example/vk/tokens.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * create new token:
+ * https://oauth.vk.com/authorize?client_id=0000000&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.52%20client_id%20=%20APP_ID
+ */
+return [
+    'app_id' => 0000000,
+    'photos' => '93693e525b116db3ca539e6298f61432f0be47e83579e36dc0261ab936eba0c3d6fb2e153dabe93d57344',
+];
