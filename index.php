395db544179ce2a60be904bfd9acb959a1ce9be9
hemonugi <hemonugi@sakh.com>
2019-07-15 23:29:22 +1100

    add method for save pictures (from the album saved pictures)

--- index.php
+++ index.php
@@ -12,23 +12,34 @@
 /**
  * @param string $method
  * @param string $methodName
- * @param array $params
  * @return string
  */
-function getOfVkApi(string $method, string $methodName, $params = []) {
+function getOfVkApi(string $method, string $methodName) {
     $vkToken = include "config/vk/tokens.php";
-    return "https://api.vk.com/method/$method.$methodName?v=5.101&access_token=$vkToken[$method]";
+    return "https://api.vk.com/method/$method.$methodName?album_id=saved&count=1000&v=5.101&access_token=$vkToken[$method]";
 }
 
-function getFriendsOnline() {
+function savePictures() {
     $ch = curl_init();
 
-    curl_setopt(
-        $ch,
-        CURLOPT_URL,
-        getOfVkApi('friends', 'getOnline')
+    curl_setopt_array(
+        $ch, [
+            CURLOPT_URL => getOfVkApi('photos', 'get'),
+            CURLOPT_RETURNTRANSFER => true
+        ]
     );
 
-    curl_exec($ch);
+    $photos = json_decode(curl_exec($ch), true)['response']['items'];
+
+    foreach ($photos as $photo) {
+        $photoUrl = array_pop($photo['sizes'])['url'];
+
+        $content = file_get_contents($photoUrl);
+        file_put_contents('pictures/'.pathinfo($photoUrl)['basename'], $content);
+        echo "Download:  $photoUrl \n";
+    }
+
     curl_close($ch);
 }
+
+savePictures();
